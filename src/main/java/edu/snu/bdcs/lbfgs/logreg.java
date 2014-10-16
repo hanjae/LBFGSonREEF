f1b3f9e75f0da4b145c9982890b92de3720cd15a
Jaehyun Han <lovelyhyun@gmail.com>
2014-10-16 13:14:07 +0900

    Add data loading part

--- src/main/java/edu/snu/bdcs/lbfgs/logreg.java
+++ src/main/java/edu/snu/bdcs/lbfgs/logreg.java
@@ -15,9 +15,11 @@ public class logreg {
 
     private static final int NUM_EPOCHS = 10000;
     private static final double LEARNING_RATE = 0.000001;
-    private static final int NUM_FEATURES = 3;
+    private int num_features;
+    private int num_data;
 
-
+    private double[][] x;
+    private double[] y;
     // Theta
     private double[] weights;
 
@@ -36,15 +38,33 @@ public class logreg {
     }
 
     private void trainLogisticRegression() {
-        double[] newWeights = new double[NUM_FEATURES];
+        double[] newWeights = new double[num_features];
 
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // write your code here
-        System.out.println("ABCD");
+    public void loadDataset() throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("binary.csv"));
+        // Skip the first line since it is header
+        scanner.nextLine();
+        int i = 0;
+        x = new double[1000][3];
+        y = new double[1000];
+        while(scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            // parse data y,x0,x1,x2
+            String[] realData = line.split(",");
+            y[i] = Double.parseDouble(realData[0]);
+            num_features = realData.length - 1;
+            for (int j = 1; j < realData.length; j++) {
+                x[i][j-1] = Double.parseDouble(realData[j]);
+            }
+            i++;
+        }
+        num_data = i;
     }
-
-
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("ABCD");
+        logreg engine = new logreg();
+        engine.loadDataset();
+    }
 }
